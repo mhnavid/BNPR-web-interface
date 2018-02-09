80131704d4bdc4dd5e7fc9f730c28ebf3c27ee1f
mhnavid <hasan.nvd@gmail.com>
2018-02-09 18:56:33 +0600

    5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-Dropzone.options.myUpload = {
-  uploadMultiple: false;
-};
+function showImg(n) {
+  var image = document.getElementById("car-big-image");
+
+  if (n == 1){
+    image.src = "img/1.jpg";
+  }
+  else if (n == 2){
+    image.src = "img/2.jpg";
+  }
+  else if (n == 3){
+      image.src = "img/3.jpg";
+  }
+  else if (n == 4){
+      image.src = "img/4.jpg";
+  }
+}
