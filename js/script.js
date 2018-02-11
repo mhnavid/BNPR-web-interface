4eec62f22779c4a68db8d54bfce9023aed7ba283
mhnavid <hasan.nvd@gmail.com>
2018-02-11 15:09:21 +0600

    7

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-function carPlateUpper() {
-    return "ঢাকা মেট্রো - গ";
-}
-
-function carPlateLower() {
-    return "৩৫ - ৭৬৭০"
-}
-
-document.getElementById("carPlateUpper").innerHTML = carPlateUpper();
-document.getElementById("carPlateLower").innerHTML = carPlateLower()
-
 function showImg(n) {
   var image = document.getElementById("car-big-image");
 
@@ -25,3 +14,35 @@ function showImg(n) {
       image.src = "img/4.jpg";
   }
 }
+
+function fileValidation(){
+    var fileInput = document.getElementById('uploadImage');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+    if(!allowedExtensions.exec(filePath)){
+        alert('Please upload file having extensions .jpeg/.jpg/.png only.');
+        fileInput.value = '';
+        return false;
+    }else{
+        //Image preview
+        var image = document.getElementById("car-big-image");
+        if (fileInput.files && fileInput.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                image.src = e.target.result;
+            };
+            reader.readAsDataURL(fileInput.files[0]);
+        }
+    }
+}
+
+function carPlateUpper() {
+    return "ঢাকা মেট্রো - গ";
+}
+
+function carPlateLower() {
+    return "৩৫ - ৭৬৭০";
+}
+
+document.getElementById("carPlateUpper").innerHTML = carPlateUpper();
+document.getElementById("carPlateLower").innerHTML = carPlateLower();
